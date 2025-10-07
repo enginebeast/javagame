6ae63eaad087450716db9046bd53c16b1e7e80cc
Lee Junghwan <well0914@naver.com>
2025-10-08 02:51:07 +0900

    Add Friend

--- src/javagame/Javagame.java
+++ src/javagame/Javagame.java
@@ -1,5 +1,61 @@
 package javagame;
 
-public class Javagame {
+class Friend{
+	String name;
+	int lv;
+	int exp;
+	int hp;
+	int atk;
+	
+	void print() {
+		System.out.println("Class: " + name);
+		System.out.println("Level: " + lv);
+		System.out.println("exp: " + exp);
+		System.out.println("HP: " + hp);
+		System.out.println("ATK: " + atk);
+		System.out.println("");
+	}
+	
+	void levelup(int level) {
+		lv += level;
+		hp += hp/10;
+		atk += atk/10;
+		
+		System.out.println(name + " level up!");
+		System.out.println("class: " + name);
+		System.out.println("Level: " + lv);
+		System.out.println("exp: " + exp);
+		System.out.println("HP: " + hp);
+		System.out.println("ATK: " + atk);
+	}
+}
 
+class Enemy{
+	int lv;
+	int hp;
+	int atk;
+}
+
+public class Javagame {
+	public static void main(String[] args) {
+		Friend warrior = new Friend();
+		Friend hacker = new Friend();
+		
+		warrior.name = "Warrior";
+		warrior.lv = 1;
+		warrior.exp = 0;
+		warrior.hp = 100;
+		warrior.atk = 100;
+		
+		hacker.name = "Hacker";
+		hacker.lv = 1;
+		hacker.exp = 0;
+		hacker.hp = 100;
+		hacker.atk = 10;
+		
+		warrior.print();
+		hacker.print();
+		
+		hacker.levelup(1);
+	}
 }
