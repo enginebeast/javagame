e35cb62fe6a19ca3f9b868d95a723c233441efb4
Lee Junghwan <well0914@naver.com>
2025-10-08 23:52:51 +0900

    get exp

--- src/javagame/Javagame.java
+++ src/javagame/Javagame.java
@@ -1,6 +1,8 @@
 package javagame;
 
 class Friend{
+	static int[] max_exp = {0, 100, 200, 300 ,400, 500, 800, 900};
+	
 	String name;
 	int lv;
 	int exp;
@@ -12,17 +14,55 @@ class Friend{
 		System.out.println("Level: " + lv);
 		System.out.println("exp: " + exp);
 		System.out.println("HP: " + hp);
-		System.out.println("ATK: " + atk);
-		System.out.println("");
+		System.out.println("ATK: " + atk + "\n");
 	}
 	
-	void levelup(int level) {
+	int[] cal_exp(int get_exp) {
+		//Already max level
+		if(lv >= 10) {
+			System.out.println(name + "is already max level!");
+			return new int[] {lv, 0};
+		}
+		
+		//Not already max level
+		else 
+			System.out.println(name + " get " + get_exp + "exp!\n");
+		
+		//Got exp is lack
+		if(exp + get_exp < max_exp[lv]) 
+			return new int[] {lv, exp + get_exp};
+
+		
+		//Character can level up!
+		int remain_exp = exp + get_exp;
+		for(int i = lv; i<11; i++) {
+			//Now max level
+			if (i == 10) {
+				levelup(i, 0);
+				return new int[] {i, 0};
+				}
+				
+			//Calculate end
+			else if(remain_exp < max_exp[i]) {
+				levelup(i, 0);
+				return new int[] {i, remain_exp};
+				}
+			
+			else
+				remain_exp -= max_exp[i];
+			}
+		
+		//Safe gear
+		return new int[] {lv, exp};
+		}
+	
+	void levelup(int level, int new_exp) {
 		lv += level;
+		exp = new_exp;
 		hp += hp/10;
 		atk += atk/10;
 		
 		System.out.println(name + " level up!");
-		System.out.println("class: " + name);
 		System.out.println("Level: " + lv);
 		System.out.println("exp: " + exp);
 		System.out.println("HP: " + hp);
@@ -30,11 +70,13 @@ class Friend{
 	}
 }
 
+
 class Enemy{
 	int lv;
 	int hp;
 	int atk;
 }
+
 
 public class Javagame {
 	public static void main(String[] args) {
@@ -56,6 +98,6 @@ public class Javagame {
 		warrior.print();
 		hacker.print();
 		
-		hacker.levelup(1);
+		hacker.cal_exp(300);
 	}
 }
