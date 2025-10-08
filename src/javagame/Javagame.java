4a462bed12a4e20c5b88213cee4b995f7b747300
Lee Junghwan <well0914@naver.com>
2025-10-09 04:26:53 +0900

    Add party

--- src/javagame/Javagame.java
+++ src/javagame/Javagame.java
@@ -1,5 +1,7 @@
 package javagame;
 
+
+// Friend class
 class Friend{
 	static int[] max_exp = {0, 100, 200, 300 ,400, 500, 800, 900};
 	
@@ -9,20 +11,23 @@ class Friend{
 	int hp;
 	int atk;
 	
-	void print() {
-		System.out.println("Class: " + name);
-		System.out.println("Level: " + lv);
-		System.out.println("exp: " + exp);
-		System.out.println("HP: " + hp);
-		System.out.println("ATK: " + atk + "\n");
-	}
+	void print_party() {
+		System.out.printf("%-10s%-10s\n", "Class: ", name);
+		System.out.printf("%-10s%-10d\n", "Level: ", lv);
+		System.out.printf("%-10s%-10d\n", "exp: ", exp);
+		System.out.printf("%-10s%-10d\n", "HP: ", hp);
+		System.out.printf("%-10s%-10d\n", "ATK: ", atk);
+		System.out.printf("\n");
+		}
 	
+	
+	// Calculate exp method
 	int[] cal_exp(int get_exp) {
 		//Already max level
 		if(lv >= 10) {
 			System.out.println(name + "is already max level!");
 			return new int[] {lv, 0};
-		}
+			}
 		
 		//Not already max level
 		else 
@@ -44,8 +49,8 @@ class Friend{
 				
 			//Calculate end
 			else if(remain_exp < max_exp[i]) {
-				levelup(i, 0);
-				return new int[] {i, remain_exp};
+				levelup(i - 1, remain_exp);
+				return new int[] {i - 1, remain_exp};
 				}
 			
 			else
@@ -56,11 +61,13 @@ class Friend{
 		return new int[] {lv, exp};
 		}
 	
+	
 	void levelup(int level, int new_exp) {
+		int saved_level = level - lv;
 		lv += level;
 		exp = new_exp;
-		hp += hp/10;
-		atk += atk/10;
+		hp += hp/10*saved_level;
+		atk += atk/10*saved_level;
 		
 		System.out.println(name + " level up!");
 		System.out.println("Level: " + lv);
@@ -78,8 +85,10 @@ class Enemy{
 }
 
 
+//Main
 public class Javagame {
 	public static void main(String[] args) {
+		Friend party[] = new Friend[4];
 		Friend warrior = new Friend();
 		Friend hacker = new Friend();
 		
@@ -88,16 +97,18 @@ public class Javagame {
 		warrior.exp = 0;
 		warrior.hp = 100;
 		warrior.atk = 100;
+		party[0] = warrior;
 		
 		hacker.name = "Hacker";
 		hacker.lv = 1;
 		hacker.exp = 0;
 		hacker.hp = 100;
 		hacker.atk = 10;
+		party[1] = hacker;
 		
-		warrior.print();
-		hacker.print();
+		warrior.print_party();
+		hacker.print_party();
 		
-		hacker.cal_exp(300);
+		hacker.cal_exp(450);
 	}
 }
